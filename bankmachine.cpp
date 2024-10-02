3710fb3c323ff316944b3fd6eb11fc7859c9d2a6
RhysticPatrol <stevenswilson7798@gmail.com>
2024-10-01 21:36:02 -0400

    Update bankmachine.cpp

--- bankmachine.cpp
+++ bankmachine.cpp
@@ -15,13 +15,19 @@ double t;
 cout << "This program is to help you calculate your balance in your savings account.";
 cout << "\nFirst, we will need you to enter your interest rate:";
 cin >> Rate;
+Rate /= 100;
 cout << "\nNext, we will need your balance in your savings account:";
 cin >> Principle;
 cout << "\nAlright, last thing! We need the number of times interest is compounded:";
 cout << "\n(example if it is done quartly enter in 4)";
 cin >> t;
-cout << "Now we will find your new balance!:\n";
-cout << Principle * (pow(( 1 + (Rate/t)),t));
+cout << "\nInterest Rate:" << Rate;
+cout << "\nTimes Compounded:" << t;
+cout << "\nPrinciple:" << Principle;
+ 
+double amount = Principle * (pow(( 1 + (Rate/t)),t));
 
+cout << "\nInterest:" << amount - Principle;
+cout << "\nAmount in Savings:" << amount;
 return 0;
 }
